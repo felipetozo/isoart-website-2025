3ed3866251c241e233a1f70f15aa4cf9169742e9
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-06-13 00:06:13 -0300

    gridsolucoes gsap corrected

--- src/app/components/SolucoesGrid/SolucoesGrid.tsx
+++ src/app/components/SolucoesGrid/SolucoesGrid.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Link from 'next/link';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -24,7 +25,7 @@ function SolucoesGrid() {
             force3D: true,
             scrollTrigger: {
                 trigger: section,
-                markers: true,
+                markers: false,
                 start: 'top top',
                 pin: true,
                 scrub: 1,
@@ -57,74 +58,84 @@ function SolucoesGrid() {
                     </div>
                     <div className={styles.SolucoesGridGrid} ref={gridRef}>
                         <div className={styles.SolucoesGridCard}>
-                            <div className={styles.SolucoesGridCardImage}>
-                                <Image
-                                    src={'/img/SolucoesGrid/telhas.jpg'}
-                                    alt="Soluções Construtivas Isotérmicas"
-                                    width={1000}
-                                    height={700}
-                                />
-                            </div>
-                            <div className={styles.SolucoesGridCardContainer}>
-                                <h5>Soluções Construtivas Isotérmicas</h5>
-                                <p>Telhas e painéis isotérmicos em PIR e EPS</p>
-                            </div>
+                            <Link href="/categorias/telhas-e-paineis">
+                                <div className={styles.SolucoesGridCardImage}>
+                                    <Image
+                                        src={'/img/SolucoesGrid/telhas.jpg'}
+                                        alt="Soluções Construtivas Isotérmicas"
+                                        width={1000}
+                                        height={700}
+                                    />
+                                </div>
+                                <div className={styles.SolucoesGridCardContainer}>
+                                    <h5>Soluções Construtivas Isotérmicas</h5>
+                                    <p>Telhas e painéis isotérmicos em PIR e EPS</p>
+                                </div>
+                            </Link>
                         </div>
                         <div className={styles.SolucoesGridCard}>
-                            <div className={styles.SolucoesGridCardImage}>
-                                <Image
-                                    src={'/img/SolucoesGrid/construcaocivil.jpg'}
-                                    alt="Construção Civil"
-                                    width={1000}
-                                    height={700}
-                                />
-                            </div>
-                            <div className={styles.SolucoesGridCardContainer}>
-                                <h5>Construção Civil</h5>
-                                <p>Lajes unidirecionais, bidirecionais e EPS para lajes pré-fabricadas</p>
-                            </div>
+                            <Link href="/categorias/construcao-civil">
+                                <div className={styles.SolucoesGridCardImage}>
+                                    <Image
+                                        src={'/img/SolucoesGrid/construcaocivil.jpg'}
+                                        alt="Construção Civil"
+                                        width={1000}
+                                        height={700}
+                                    />
+                                </div>
+                                <div className={styles.SolucoesGridCardContainer}>
+                                    <h5>Construção Civil</h5>
+                                    <p>Lajes unidirecionais, bidirecionais e EPS para lajes pré-fabricadas</p>
+                                </div>
+                            </Link>
                         </div>
                         <div className={styles.SolucoesGridCard}>
-                            <div className={styles.SolucoesGridCardImage}>
-                                <Image
-                                    src={'/img/SolucoesGrid/molduras.jpg'}
-                                    alt="Decoração"
-                                    width={1000}
-                                    height={700}
-                                />
-                            </div>
-                            <div className={styles.SolucoesGridCardContainer}>
-                                <h5>Decoração</h5>
-                                <p>Aplicação em diversos acabamentos</p>
-                            </div>
+                            <Link href="/categorias/molduras-decorativas">
+                                <div className={styles.SolucoesGridCardImage}>
+                                    <Image
+                                        src={'/img/SolucoesGrid/molduras.jpg'}
+                                        alt="Decoração"
+                                        width={1000}
+                                        height={700}
+                                    />
+                                </div>
+                                <div className={styles.SolucoesGridCardContainer}>
+                                    <h5>Decoração</h5>
+                                    <p>Aplicação em diversos acabamentos</p>
+                                </div>
+                            </Link>
                         </div>
                         <div className={styles.SolucoesGridCard}>
-                            <div className={styles.SolucoesGridCardImage}>
-                                <Image
-                                    src={'/img/SolucoesGrid/molduras.jpg'}
-                                    alt="Forros em EPS"
-                                    width={1000}
-                                    height={700}
-                                />
-                            </div>
-                            <div className={styles.SolucoesGridCardContainer}>
-                                <h5>Forros em EPS</h5>
-                                <p>Aplicação em diversos forros</p>
-                            </div>
+                            <Link href="/categorias/forros">
+                                <div className={styles.SolucoesGridCardImage}>
+                                    <Image
+                                        src={'/img/SolucoesGrid/molduras.jpg'}
+                                        alt="Forros em EPS"
+                                        width={1000}
+                                        height={700}
+                                    />
+                                </div>
+                                <div className={styles.SolucoesGridCardContainer}>
+                                    <h5>Forros em EPS</h5>
+                                    <p>Aplicação em diversos forros</p>
+                                </div>
+                            </Link>
                         </div>
                         <div className={styles.SolucoesGridCard}>
-                            <div className={styles.SolucoesGridCardImage}>
-                                <Image
-                                    src={'/img/SolucoesGrid/embalagens.jpg'}
-                                    alt="Embalagens"
-                                    width={1000}
-                                    height={700}
-                                />
-                            </div>
-                            <div className={styles.SolucoesGridCardContainer}>
-                                <h5>Embalagens</h5>
-                                <p>Proteção ideal para embalar o seu produto</p>
-                            </div>
+                            <Link href="/categorias/embalagens-em-eps">
+                                <div className={styles.SolucoesGridCardImage}>
+                                    <Image
+                                        src={'/img/SolucoesGrid/embalagens.jpg'}
+                                        alt="Embalagens"
+                                        width={1000}
+                                        height={700}
+                                    />
+                                </div>
+                                <div className={styles.SolucoesGridCardContainer}>
+                                    <h5>Embalagens</h5>
+                                    <p>Proteção ideal para embalar o seu produto</p>
+                                </div>
+                            </Link>
                         </div>
                     </div>
                 </div>
