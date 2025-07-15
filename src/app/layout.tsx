03c48d705932d7372ea24c594bd951c25d6a13bb
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-07-15 13:44:12 -0300

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Red_Hat_Display, Inter } from "next/font/google";
 import "./globals.css";
 import MainNav from "./components/MainNav/MainNav";
 import Footer from "./components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const RedHat = Red_Hat_Display({
   variable: "--font-RedHat",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <MainNav />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
