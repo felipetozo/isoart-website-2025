66fe4233979342230dc1b9fdc703d254b51486c3
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-07-24 15:45:59 -0300

    Images partially updated

--- src/app/sobre-eps-pir/page.tsx
+++ src/app/sobre-eps-pir/page.tsx
@@ -4,6 +4,13 @@ import Button from "@/app/views/UI/Button";
 import Sustentabilidade from "@/app/components/Sustentabilidade/Sustentabilidade";
 import { CheckCircle } from "lucide-react";
 import { FlameOff } from "tabler-icons-react";
+import Image from "next/image";
+
+const incendioBlocos = [
+  { number: '01', destaque: 'Impede a propagação', legenda:'A camada carbonizada retarda significativamente a queima do material, contendo o foco do incêndio e evitando que as chamas se espalhem pela estrutura' },
+  { number: '02', destaque: 'Protege o núcleo', legenda:'Isola o restante do painel do calor intenso, mantendo a integridade estrutural por mais tempo' },
+  { number: '03', destaque: 'Baixa emissão de fumaça', legenda:'Contribui para manter as rotas de fuga mais visíveis e seguras' },
+];
 
 export default function SobreEpsPirPage() {
   return (
@@ -45,7 +52,7 @@ export default function SobreEpsPirPage() {
               O Poliestireno Expandido (EPS), conhecido popularmente como Isopor, é um material leve e versátil, amplamente utilizado por suas propriedades de isolamento térmico e acústico. Além disso, é 100% reciclável e não emite substâncias prejudiciais ao meio ambiente, sendo uma escolha sustentável para diversas aplicações.
             </p>
             <div className={styles.epsImageContainer}>
-              <img src="/img/geral/exemplo.png" alt="EPS exemplo" className={styles.epsImage} />
+              <img src="/img/geral/eps-perolas.webp" alt="Pérola de EPS - Isoart" className={styles.epsImage} />
             </div>
             <h2 className={styles.epsSubtitle}>Principais benefícios do EPS:</h2>
             <div className={styles.benefitsContainer}>
@@ -95,7 +102,7 @@ export default function SobreEpsPirPage() {
               ao fogo, sendo essencial para projetos industriais, comerciais e residenciais.
             </p>
             <div className={styles.pirImageContainer}>
-              <img src="/img/geral/exemplo.png" alt="PIR exemplo" className={styles.pirImage} />
+              <img src="/img/geral/pir-block.png" alt="PIR exemplo" className={styles.pirImage} />
             </div>
             <h2 className={styles.pirSubtitle}>Principais benefícios do PIR:</h2>
             <div className={styles.benefitsContainer}>
@@ -201,20 +208,122 @@ export default function SobreEpsPirPage() {
         </div>
       </section>
 
+      {/* Proteção contra incêndio */}
       <section className={styles.IncendioSection}>
         <div className={styles.IncendioWrapper}>
-          <div className={styles.IncendioTitle}>
-            <div className={styles.IncendioIcone}>
-              <FlameOff />
-            </div>
+          <div className={styles.IncendioHeader}>
+            <span className={styles.IncendioTitle}>
+              <span className={styles.IncendioTitleIcon}>
+                <FlameOff />
+              </span>
+              <h3>
+                PIR: O escudo protetor contra a propagação de incêndios
+              </h3>
+            </span>
+            <span className={styles.IncendioHeaderDescription}>
+                A performance do Poliisocianurato (PIR) em situações de incêndio é um de seus maiores
+                diferenciais, oferecendo uma camada de proteção passiva essencial para qualquer edificação.
+                Diferente de outros materiais, o PIR não contribui para a propagação de chamas.
+                <br /><br />
+                Quando exposto ao fogo, o núcleo do painel de PIR reage de forma única: ele forma uma
+                camada carbonizada estável e isolante em sua superfície. Essa barreira de carbono age como
+                um escudo protetor, realizando três funções cruciais:
+            </span>
           </div>
-
+          <div className={styles.IncendioBlocos}>
+            {incendioBlocos.map((b, i) =>
+              <div className={styles.IncendioBloco} key={i}>
+                <span className={styles.IncendioBlocoNumber}>
+                  <h4>
+                    {b.number}
+                  </h4>
+                </span>
+                <span>
+                  <strong>{b.destaque}:</strong> {b.legenda}
+                </span>
+              </div>
+            )}
+          </div>
+          <div className={styles.IncendioImages}>
+            <span>
+              <Image
+                src="/img/incendio/pir-incendio-01.jpg"
+                alt="Proteção contra incêndio com PIR"
+                width={600}
+                height={818}
+                priority
+              />
+            </span>
+            <span>
+              <Image
+                src="/img/incendio/pir-incendio-02.jpg"
+                alt="Proteção contra incêndio com PIR"
+                width={600}
+                height={818}
+                priority
+              />
+            </span>
+            <span>
+              <Image
+                src="/img/incendio/pir-incendio-03.jpg"
+                alt="Proteção contra incêndio com PIR"
+                width={600}
+                height={818}
+                priority
+              />
+            </span>
+          </div>
+          <span className={styles.IncendioTeste}>
+            O teste demonstrado nas imagens comprova essa eficiência. Mesmo sob a ação direta de um queimador de 30kW por 30 minutos, o fogo ficou restrito
+            à área de contato. Ao final, o resultado foi apenas uma carbonização superficial, com o restante do painel intacto e sem qualquer propagação de fogo,
+            demonstrando por que o PIR é a escolha mais segura e inteligente para o seu projeto.
+          </span>
         </div>
       </section>
-     
 
-
-
+      {/* Linha de produção */}
+      <section className={styles.linhaProducaoSection}>
+        <div className={styles.linhaProducaoWrapper}>
+          <h3>
+            Produção automatizada para máximo eficiência
+          </h3>
+          <span>
+            As telhas e painéis Isoart PIR são fabricadas em linha contínua e totalmente automatizada, garantindo alta precisão, qualidade e sustentabilidade.
+            Com núcleo isolante de Poliisocianurato (PIR) e revestimento em chapas de aço galvalume, o processo inclui injeção de alta pressão, prensagem contínua
+            e corte automatizado, assegurando excelente adesão, acabamento e padronização. Todos os produtos são resfriados, empilhados e embalados automaticamente,
+            prontos para expedição.
+          </span>
+          <div className={styles.linhaProducaoImages}>
+            <span>
+              <Image
+                src="/img/fabrica/fabrica-01.jpg"
+                alt="Proteção contra incêndio com PIR"
+                width={600}
+                height={818}
+                priority
+              />
+            </span>
+            <span>
+              <Image
+                src="/img/fabrica/fabrica-02.jpg"
+                alt="Proteção contra incêndio com PIR"
+                width={600}
+                height={818}
+                priority
+              />
+            </span>
+            <span>
+              <Image
+                src="/img/fabrica/fabrica-03.jpg"
+                alt="Proteção contra incêndio com PIR"
+                width={600}
+                height={818}
+                priority
+              />
+            </span>
+          </div>
+        </div>
+      </section>
 
     </div>
   );
