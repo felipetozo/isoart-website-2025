edfc24453dae36ec8f7c3d5696398419349da6df
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-08-11 01:20:06 -0300

    termos de privacidade adicionados

---
+++ src/app/privacidade/page.tsx
@@ -0,0 +1,167 @@
+'use client';
+import React from 'react';
+import styles from './page.module.css';
+
+const PrivacidadePage: React.FC = () => {
+    return (
+        <>
+            {/* Hero Section */}
+            <section className={styles['privacidade-hero-section']}>
+                <div className={styles['privacidade-hero-wrapper']}>
+                    <h3>Política de Privacidade</h3>
+                    <p>Conheça como a ISOART protege e utiliza seus dados pessoais:</p>
+                </div>
+            </section>
+
+            {/* Content Section */}
+            <section className={styles['privacidade-content-section']}>
+                <div className={styles['privacidade-content-wrapper']}>
+                    <div className={styles['privacidade-content']}>
+                        <p>
+                            A Isoart Indústria de Produtos Térmicos e Construtivos LTDA. e empresas filiais de seu grupo econômico (em conjunto "ISOART") utiliza a tecnologia de cookies para oferecer um serviço melhor e mais personalizado. A presente Política de Cookies ("Política") é parte integrante da nossa Política de Privacidade.
+                        </p>
+
+                        <p>
+                            Os termos dessa Política aplicam-se a todos os Usuários e visitantes do nosso website (doravante denominado "Site") com o objetivo de informar, de forma clara e objetiva sobre o uso de cookies nos mesmos.
+                        </p>
+
+                        <p>
+                            Para mais informações sobre como a ISOART realiza o Tratamento dos seus Dados Pessoais, consulte a nossa Política de Privacidade no site www.isoart.com.br.
+                        </p>
+
+                        <p>
+                            Ao acessar nosso Site pela primeira vez, os Usuários e visitantes são informados através de um aviso de texto ("banner") referente a prática de cookies dos Sites.
+                        </p>
+
+                        <h4>Definições</h4>
+                        <p>
+                            Para os fins desta Política, devem ser consideradas as seguintes definições e descrições para seu melhor entendimento, quando referidas em letra maiúscula ou minúscula, plural ou singular, com ou sem negrito:
+                        </p>
+
+                        <p>
+                            <strong>Dados Pessoais:</strong> qualquer informação relacionada a pessoa natural identificada ou identificável;
+                        </p>
+
+                        <p>
+                            <strong>Tratamento:</strong> toda a operação realizada com Dados Pessoais, como as que se referem a coleta, produção, recepção, classificação, utilização, acesso, reprodução, transmissão, distribuição, processamento, arquivamento, armazenamento, eliminação, avaliação ou controle da informação, modificação, comunicação, transferência, difusão ou extração;
+                        </p>
+
+                        <p>
+                            <strong>Usuário (ou Usuários):</strong> todas as pessoas físicas ou jurídicas cadastradas que interajam com as funcionalidades oferecidas pela ISOART;
+                        </p>
+
+                        <p>
+                            <strong>Logs:</strong> registros das atividades de visitantes e Usuários verificadas no Site.
+                        </p>
+
+                        <h4>O que são cookies?</h4>
+                        <p>
+                            Os "cookies" são considerados pequenos arquivos, de texto ou imagem, captados em nossos Sites, armazenados nos dispositivos utilizados pelos Usuários que os acessam (por exemplo, computadores, tablets, smartphones etc.), onde são registrados para a posterior retransmissão aos mesmos Sites quando este usuário volta a visitá-los.
+                        </p>
+
+                        <h4>Para que servem?</h4>
+                        <p>
+                            Os Cookies desempenham diversas funções diferentes, tais como permitir que você navegue entre as páginas de maneira eficiente, armazenar suas preferências e, em geral, melhorar sua experiência de navegação.
+                        </p>
+
+                        <p>
+                            Eles também podem ajudar a personalizar os anúncios publicitários exibidos durante a navegação e as atividades de marketing direcionadas aos Usuários ou visitantes, garantindo que eles sejam enviados de acordo com seus interesses e preferências.
+                        </p>
+
+                        <h4>Gerenciando suas configurações de cookies</h4>
+                        <p>
+                            A maioria dos navegadores de Internet são configurados por padrão para aceitar automaticamente a instalação de cookies. No entanto, a maioria dos navegadores possibilita o gerenciamento de cookies para se enquadrar às suas preferências.
+                        </p>
+
+                        <p>
+                            Os Usuários e visitantes também podem remover regularmente todos os cookies que já estão nos seus dispositivos, limpando o histórico do seu navegador. Isso removerá todos os cookies de todos os sites visitados. Contudo, esteja ciente de que, se você desabilitar os cookies que utilizamos no seu navegador, é possível que certas seções ou recursos dos nossos Sites não funcionem.
+                        </p>
+
+                        <p>
+                            Caso os Usuários ou visitantes usem dispositivos diferentes para visualizar e acessar o nosso Site devem assegurar-se de que cada navegador de cada dispositivo está ajustado para atender suas preferências quanto aos cookies.
+                        </p>
+
+                        <p>
+                            Para o gerenciamento de cookies, os meios para configurar cada navegador são diferentes. Dessa maneira, segue abaixo uma lista dos métodos oferecidos pelos principais navegadores para permitir que os Usuários ou visitantes gerenciem as configurações de cookies em seu navegador:
+                        </p>
+
+                        <ul>
+                            <li>Para o Internet Explorer™: <a href="https://support.microsoft.com/en-gb/help/17442/windows-internet-explorer-delete-manage-cookies?ocid=IE10_about_cookies" target="_blank" rel="noopener noreferrer">https://support.microsoft.com/en-gb/help/17442/windows-internet-explorer-delete-manage-cookies?ocid=IE10_about_cookies</a></li>
+                            <li>Para o Safari™: <a href="http://support.apple.com/kb/HT1677?viewlocale=en_US" target="_blank" rel="noopener noreferrer">http://support.apple.com/kb/HT1677?viewlocale=en_US</a></li>
+                            <li>Para o Google Chrome™: <a href="https://support.google.com/chrome/answer/95647?hl=en" target="_blank" rel="noopener noreferrer">https://support.google.com/chrome/answer/95647?hl=en</a></li>
+                            <li>Para o Firefox™: <a href="https://support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences" target="_blank" rel="noopener noreferrer">https://support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences</a></li>
+                            <li>Para o Opera™: <a href="http://help.opera.com/Windows/10.20/en/cookies.html" target="_blank" rel="noopener noreferrer">http://help.opera.com/Windows/10.20/en/cookies.html</a></li>
+                        </ul>
+
+                        <p>
+                            Para mais informações sobre a configuração de cookies, acesse: <a href="http://www.aboutcookies.org/" target="_blank" rel="noopener noreferrer">http://www.aboutcookies.org/</a>
+                        </p>
+
+                        <p>
+                            Informamos que essa lista não contém todos os navegadores, consulte as seções "Configurações" ou "Suporte" de seu navegador.
+                        </p>
+
+                        <p>
+                            Desta forma, você poderá revogar o consentimento de utilização de dados a qualquer momento, sem que, no entanto, torne ilegítimo o tratamento destes com base nesse consentimento, até a data da efetiva revogação.
+                        </p>
+
+                        <h4>Cookies utilizados e suas finalidades</h4>
+                        <p>
+                            Os Cookies utilizados pelos nossos Sites executam as seguintes funções, conforme mencionado abaixo:
+                        </p>
+
+                        <h4>Cookies Funcionais</h4>
+                        <p>
+                            Os Cookies funcionais permitem-nos registrar as opções que os Usuários e visitantes selecionam em nossos Sites, por exemplo, o idioma, lembrar o usuário/senha e as preferências. Com estas informações podemos proporcionar funcionalidades mais personalizadas aos Usuários e visitantes que navegam em nossos Sites.
+                        </p>
+
+                        <h4>Cookies de Análise e Performance</h4>
+                        <p>
+                            Esses Cookies nos permitem coletar informações sobre o uso que você faz do nosso site, e são utilizados para nos ajudar a entender como você interage com o nosso site, para que possamos oferecer uma melhor experiência do usuário– por exemplo, avaliar o desempenho do nosso site, ou testar diferentes ideias de design para o site. Nós podemos trabalhar com terceiros, para realizar esses serviços para nós, então esses Cookies podem ser definidos por um terceiro.
+                        </p>
+
+                        <h4>Cookies estruturais do site</h4>
+                        <p>
+                            Estes Cookies são necessários para o funcionamento do website e não podem ser desativados dos nossos sistemas. Como regra estes cookies são pré-definidos em resposta a solicitações de serviços. Em algumas situações partes do website poderão deixar de funcionar.
+                        </p>
+
+                        <h4>Cookies de marketing e publicidade</h4>
+                        <p>
+                            Estes Cookies são utilizados para publicidade comportamental online, consequentemente geram informações para que os usuários recebam conteúdos personalizados.
+                        </p>
+
+                        <p>
+                            <strong>Cookie Dart</strong> – O Google, como fornecedor de terceiros, utiliza cookies para exibir anúncios em sites. Com o Cookie Dart, o Google pode exigir anúncios com base nas visitas que o leitor fez a este e outros sites na internet. Você pode desativar o cookie Dart visitando as configurações de anúncios do Google.
+                        </p>
+
+                        <p>
+                            <strong>Web Beacons</strong> – Utilizamos esses cookies para armazenar informações. A exemplo de preferências pessoais ao visitar os sites.
+                        </p>
+
+                        <h4>Sites de terceiros</h4>
+                        <p>
+                            O site da ISOART pode manter conexões com outros sites que complementem ou contenham informações e ferramentas úteis para os visitantes, ao passo que se os visitantes e usuários acessarem, esta política de privacidade não se estende aos terceiros, não nos responsabilizando pela política de privacidade ou conteúdo daqueles.
+                        </p>
+
+                        <h4>Direitos do titular</h4>
+                        <p>
+                            São resguardados os direitos do titular dos dados, em especial: acesso aos dados; correção de dados incompletos, inexatos ou desatualizados; confirmação da existência de tratamento; anonimização, bloqueio ou eliminação de dados desnecessários, excessivos ou tratados em desconformidade com o disposto na Lei Geral de Proteção de Dados (Lei 13.709 de 14 de Agosto de 2018); portabilidade dos dados a outro fornecedor de serviço ou produto, mediante requisição expressa e observados os segredos comercial e industrial; eliminação dos dados pessoais tratados; informação de compartilhamento de dados com entidades públicas ou privadas; informação sobre as consequências por não fornecer consentimento e revogação do consentimento.
+                        </p>
+
+                        <h4>Vigência</h4>
+                        <p>
+                            Esta Política de Privacidade permanecerá em vigor por prazo indeterminado.
+                        </p>
+
+                        <h4>Entre em contato conosco</h4>
+                        <p>
+                            Se você tiver quaisquer dúvidas ou comentários sobre a presente Política de Cookies, entre em contato conosco através do nosso Canal de Relacionamento no site <a href="https://www.isoart.com.br" target="_blank" rel="noopener noreferrer">www.isoart.com.br</a>.
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default PrivacidadePage;
