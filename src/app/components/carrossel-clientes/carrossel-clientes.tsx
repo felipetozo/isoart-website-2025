09f97bcac9b519d242afe61d29f2718a729da27d
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-08-17 02:11:47 -0300

    feat: padronizar carrossel de parceiros para usar ImageCarousel
    - Converter CarrosselClientes de lista estática para ImageCarousel funcional
    - Unificar visual e funcionamento com outros carrosséis do site
    - Manter mesmo estilo mas com navegação, miniaturas e fullscreen
    - Garantir consistência visual em todas as páginas
    - Aplicar mesmo padrão de botões e controles

--- src/app/components/carrossel-clientes/carrossel-clientes.tsx
+++ src/app/components/carrossel-clientes/carrossel-clientes.tsx
@@ -1,90 +1,37 @@
 import styles from './carrossel-clientes.module.css';
-import Image from 'next/image';
+import ImageCarousel from '../image-carousel/image-carousel';
 
 function CarrosselClientes() {
+    const parceirosImages = [
+        '/img/parceiros/parceiro-8.avif',
+        '/img/parceiros/parceiro-9.avif',
+        '/img/parceiros/parceiro-10.avif',
+        '/img/parceiros/parceiro-11.avif',
+        '/img/parceiros/parceiro-12.avif',
+        '/img/parceiros/parceiro-13.avif',
+        '/img/parceiros/parceiro-14.avif',
+        '/img/parceiros/parceiro-15.avif',
+        '/img/parceiros/parceiro-16.avif'
+    ];
+
     return (
         <>
             <section className={styles['carrossel-clientes-section']} id="CarrosselClientes">
                 <div className={styles['carrossel-clientes-wrapper']}>
                     <p>Confiança dos Clientes em Nossos Parceiros</p>
-                    <ul>
-                        <li>
-                            <Image
-                                src={'/img/parceiros/parceiro-8.avif'}
-                                alt="Logotipo Isoart"
-                                width={120}
-                                height={62}
-                            />
-                        </li>
-                        <li>
-                            <Image
-                                src={'/img/parceiros/parceiro-9.avif'}
-                                alt="Logotipo Isoart"
-                                width={120}
-                                height={62}
-                            />
-                        </li>
-                        <li>
-                            <Image
-                                src={'/img/parceiros/parceiro-10.avif'}
-                                alt="Logotipo Isoart"
-                                width={120}
-                                height={62}
-                            />
-                        </li>
-                        <li>
-                            <Image
-                                src={'/img/parceiros/parceiro-11.avif'}
-                                alt="Logotipo Isoart"
-                                width={120}
-                                height={62}
-                            />
-                        </li>
-                        <li>
-                            <Image
-                                src={'/img/parceiros/parceiro-12.avif'}
-                                alt="Logotipo Isoart"
-                                width={120}
-                                height={62}
-                            />
-                        </li>
-                        <li>
-                            <Image
-                                src={'/img/parceiros/parceiro-13.avif'}
-                                alt="Logotipo Isoart"
-                                width={120}
-                                height={62}
-                            />
-                        </li>
-                        <li>
-                            <Image
-                                src={'/img/parceiros/parceiro-14.avif'}
-                                alt="Logotipo Isoart"
-                                width={120}
-                                height={62}
-                            />
-                        </li>
-                        <li>
-                            <Image
-                                src={'/img/parceiros/parceiro-15.avif'}
-                                alt="Logotipo Isoart"
-                                width={120}
-                                height={62}
-                            />
-                        </li>
-                        <li>
-                            <Image
-                                src={'/img/parceiros/parceiro-16.avif'}
-                                alt="Logotipo Isoart"
-                                width={120}
-                                height={62}
-                            />
-                        </li>
-                    </ul>
+                    
+                    <div className={styles['parceiros-carousel-container']}>
+                        <ImageCarousel 
+                            images={parceirosImages}
+                            alt="Parceiros e Clientes Isoart"
+                            width={120}
+                            height={62}
+                        />
+                    </div>
                 </div>
             </section>
         </>
     );
-};
+}
 
 export default CarrosselClientes;
