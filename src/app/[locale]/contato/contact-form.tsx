8ed727ecb38776f6f071373777e8feb287ba4d9e
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-09-03 18:48:24 -0300

    Alterado fundo do texto em  solucoes-grid

--- src/app/[locale]/contato/contact-form.tsx
+++ src/app/[locale]/contato/contact-form.tsx
@@ -313,7 +313,6 @@ export default function ContactForm({ locale }: ContactFormProps) {
                         </div>
                     </form>
                 </div>
-
             </section>
 
             <Toast
